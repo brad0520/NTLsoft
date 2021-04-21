05658e019d784cc74a9775d2005923badc2e25e7
NTL <58549685+brad0520@users.noreply.github.com>
2021-04-21 21:46:38 +0900

    NTLsoft upload

---
+++ src/main/java/com/ntlsoft/NtLsoftApplication.java
@@ -0,0 +1,13 @@
+package com.ntlsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NtLsoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NtLsoftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ntlsoft/NtLsoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ntlsoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NtLsoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
