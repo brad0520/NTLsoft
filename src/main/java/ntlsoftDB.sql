978f3f5189f180297efd4e11a0e80155c843f7d8
Brad Park <brad0135@gmail.com>
2021-04-26 22:53:02 +0900

    main page update

--- src/main/java/ntlsoftDB.sql
+++ src/main/java/ntlsoftDB.sql
@@ -141,7 +141,7 @@ loginId = 'user1',
 loginPw = 'user1',
 `name` = '유저1이름',
 nickname = '유저1별명',
-email = 'jangka512@gmail.com',
+email = 'brad0135@gmail.com',
 cellphoneNo = '01012341234';
 
 # 회원 테스트 데이터 생성
@@ -153,7 +153,7 @@ loginId = 'user2',
 loginPw = 'user2',
 `name` = '유저2이름',
 nickname = '유저2별명',
-email = 'jangka512@gmail.com',
+email = 'brad0135@gmail.com',
 cellphoneNo = '01012341234';
 
 # 회원 테스트 데이터 생성
@@ -165,7 +165,7 @@ loginId = 'user3',
 loginPw = 'user3',
 `name` = '유저3이름',
 nickname = '유저3별명',
-email = 'jangka512@gmail.com',
+email = 'brad0135@gmail.com',
 cellphoneNo = '01012341234';
 
 
